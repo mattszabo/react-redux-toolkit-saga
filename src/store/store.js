3a50c55faea74cfbc35cd90fef999f0f68dc7de0
Matt Szabo <mszabo@dius.com.au>
2021-11-26 20:07:09 +1100

    refactor: naming

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import postReducer from '../reducer/cardsSlice'
+import cardsReducer from '../reducer/cardsSlice'
 
 export const store = configureStore({
   reducer: {
-    wallet: postReducer
+    wallet: cardsReducer
   },
 })
